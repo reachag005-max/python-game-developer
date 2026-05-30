4df028dc98968d3533ee1f278a72573fdcd44459
reachag005-max <reachag005@gmail.com>
2026-05-30 11:57:21 +0100

    Add files via upload

--- 2D list homework.py
+++ 2D list homework.py
@@ -11,11 +11,11 @@ for i in range (b):
         Temp.append(x)
     matrix.append(Temp)
 
-for i in range(0,len(matrix)):
+"""for i in range(0,len(matrix)):
     for j in range(0,len(matrix[0])):
         print(matrix[i][j], end=" ")
     print("\n")
-
+"""
 
 c = int(input("enter number of rows: "))
 
@@ -27,15 +27,28 @@ for z in range (c):
         Temp.append(x)
     matrixi.append(Temp)
 
-for i in range(0,len(matrixi)):
+"""for i in range(0,len(matrixi)):
     for j in range(0,len(matrixi[0])):
         print(matrixi[i][j], end=" ")
-    print("\n")
+    print("\n")"""
 
 
 print("here is your matrix")
 
-for i in range(len(matrixy)):
+for i in range (len(matrix)):
+    Temp=[]
+    for j in range (len(matrix[0])):
+        Temp.append(matrix[i][j] + matrixi[z][y])
+        matrixy.append(Temp)
+
+print("sum matrix")
+for i in range (len(matrixy)):
+    for j in range (len(matrixy[0])):
+        print(matrixy[i][j], end =" ")
+    print("\n")
+
+
+"""for i in range(len(matrixy)):
     for j in range(len(matrixy[0])):
         print( matrix[i][j] + matrixi[z][y], end=" ")
-    print("\n")
+    print("\n")"""
