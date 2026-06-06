07baf52d9148f1284329a304b5b8e7e8196c2b8b
reachag005-max <reachag005@gmail.com>
2026-06-06 12:00:39 +0100

    Add files via upload

---
+++ truple.py
@@ -0,0 +1,56 @@
+"""t = (1,2,3,4,5,6,7,8,9,0)
+print(t)
+
+car = ("BMW","BYD","APPOLO","FERARI","GTR")
+print(car[0])
+print(car[-1])
+
+print("the total amount of cars present in cars is",len(car))
+
+for item in car:
+    print(item)
+
+print("FERARI"in car)
+print("LAMBO"in car)
+print(type("FERARI"))"""
+
+"""f1= (1,2,3,4)
+f2= (5,6,7,8)
+comb = f1 + f2
+print(comb)
+
+rep = f1*25
+print(rep)
+
+print(rep.count(2))
+print(rep.index(2))
+
+f3 = (1,2,3,4,5,6,7,8,9,0)
+print(f3)
+del f3
+print(f3)"""
+
+"""t = (1,2,3,4,5,6,7,8,9,0)
+print(t[1:6:1])
+print(t[::-2])
+print(t[:9:-1])
+print(t[6:0:2])"""
+
+t = (1,2,3,4,5,6,7,8,9,0)
+print(t)
+
+t1 = 1,2,3,4,5,6,7,8,9,0
+print(t)
+print(type(t1))
+
+t2 = ("N")
+print(type("N"))
+
+t3 = ("N",)
+print(type("N"))
+
+t4 = (20,30,40)
+a,b,c = t4
+print(a)
+print(b)
+print(c)
