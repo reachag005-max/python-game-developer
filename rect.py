bdd117d9d6d7cb6aa6f0a7a893a328344da0dd7d
reachag005-max <reachag005@gmail.com>
2026-09-13 11:30:58 +0100

    Add files via upload

--- rect.py
+++ rect.py
@@ -3,9 +3,23 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((600,400))
 
+class rectangle():
+    def __init__(self):
+        self.color = "yellow"
+        self.length = 100
+        self.width = 50
+        self.dimention = (300,400,100,50)
+        print("All rectangle properties where complete rectangle avalible to draw")
 
+    def draw(self):
+        pygame.draw.rect(screen,self.color,self.dimention)
+        print("Draw function was complete")
+
+yellowrect = rectangle()
+yellowrect.draw()
 
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
+    pygame.display.update()
